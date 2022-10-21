a7dece1019a458e3efcc5f290830cb0723bd9d40
Matt Pavelko <matt@crowdbotics.com>
2022-10-21 10:37:49 -0400

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "spotr2_37356",
   url: "https://spotr2_37356.botics.co",
   api: "https://spotr2_37356.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
